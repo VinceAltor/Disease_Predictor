12e05925c3e0296be1b57f4fad4a63434a439eec
VinceAltor <124437888+VinceAltor@users.noreply.github.com>
2023-02-06 00:05:03 +0530

    Add files via upload

--- Dp.py.py
+++ Dp.py.py
@@ -2,31 +2,79 @@
 import pickle
 import streamlit as st
 from streamlit_option_menu import option_menu
-
+import requests
+from streamlit_lottie import st_lottie
 
 # loading the saved models
+st.set_page_config(page_title="Disease Predictor", page_icon=":tada:", layout="wide")
 
 diabetes_model = pickle.load(open('C:/Users/User/Desktop/DISEASE PREDICTION SYSTEM/diabetes_model.sav', 'rb'))
 
 heart_disease_model = pickle.load(open('C:/Users/User/Desktop/DISEASE PREDICTION SYSTEM/heart_disease_model.sav','rb'))
 
+def load_lottieurl(url):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
 
+lottie_coding = load_lottieurl("https://assets7.lottiefiles.com/private_files/lf30_dd7yh7kk.json")
+lottie_coding2 = load_lottieurl("https://assets8.lottiefiles.com/private_files/lf30_g0xeklmz.json")
+lottie_codinga = load_lottieurl("https://assets7.lottiefiles.com/packages/lf20_ginda0jy.json")
+lottie_coding3 = load_lottieurl("https://assets8.lottiefiles.com/private_files/lf30_g0xeklmz.json")
 
 
 # sidebar for navigation
 with st.sidebar:
     
     selected = option_menu('Multiple Disease Prediction System',
-                          ['Diabetes Prediction',
+                          ['Home','Diabetes Prediction',
                            'Heart Disease Prediction'],
                           icons=['activity','heart','person'],
                           default_index=0)
+
+
+if (selected == 'Home'):
+
+    left_column, right_column = st.columns(2)
+
+
+    with right_column:
+        st_lottie(lottie_coding, height=200, key="coding")
+
+    new_title = '<p style="font-family:sans-serif; color:blue; font-size: 70px;"> DISEASE PREDICTOR</p>'
+    st.markdown(new_title, unsafe_allow_html=True)
+
+    new_title = '<p style="font-family:sans-serif; color:black; font-size: 25px;"> Welcome to Disease predictor!!</p>'
+    st.markdown(new_title, unsafe_allow_html=True)
+
+    new_title = '<p style="font-family:sans-serif; color:black; font-size: 25px;"> Our Machine learning based model can predict whether you are any health ailments like Diabetes and Heart Diseases or not </p>'
+    st.markdown(new_title, unsafe_allow_html=True)
+
+    new_title = '<p style="font-family:sans-serif; color:black; font-size: 25px;"> Please Check out Diabetes predictor and heart ailemnt predictor by clicking on Slidebars.</p>'
+    st.markdown(new_title, unsafe_allow_html=True)
+
+    new_title = '<p style="font-family:sans-serif; color:black; font-size: 25px;"> THANK YOU and have a nice day!! </p>'
+    st.markdown(new_title, unsafe_allow_html=True)
+
+    new_title = '<p style="font-family:sans-serif; color:black; font-size: 20px;"> Developed by: Vinay and Team</p>'
+    st.markdown(new_title, unsafe_allow_html=True)
+
+    with left_column:
+        st_lottie(lottie_codinga, height=200, key="co")
+    
+    
+
+
+
+
+
 
 # Diabetes Prediction Page
 if (selected == 'Diabetes Prediction'):
     
     # page title
-    st.title('Diabetes Prediction using ML')
+    st.title('Diabetes Prediction')
     
     
     # getting the input data from the user
@@ -66,10 +114,11 @@ if (selected == 'Diabetes Prediction'):
         diab_prediction = diabetes_model.predict([[Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]])
         
         if (diab_prediction[0] == 1):
-          diab_diagnosis = 'The person is diabetic'
+          diab_diagnosis = 'You are diagnosed with diabetes, Please consult nearby doctor'
         else:
-          diab_diagnosis = 'The person is not diabetic'
-        
+          diab_diagnosis = 'You are not diagnosed with diabetes'
+
+    st_lottie(lottie_coding2, height=200, key="cod")    
     st.success(diab_diagnosis)
 
 
@@ -79,7 +128,7 @@ if (selected == 'Diabetes Prediction'):
 if (selected == 'Heart Disease Prediction'):
     
     # page title
-    st.title('Heart Disease Prediction using ML')
+    st.title('Heart Disease Prediction')
     
 
     
@@ -125,5 +174,5 @@ if (selected == 'Heart Disease Prediction'):
           heart_diagnosis = 'The person is having heart disease'
         else:
           heart_diagnosis = 'The person does not have any heart disease'
-        
+    st_lottie(lottie_coding3, height=200, key="coder")    
     st.success(heart_diagnosis)
